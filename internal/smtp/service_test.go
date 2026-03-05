62b61d7034e1cd5f08cfd408fd3bcfb11b8ed803
Andy Esser <andy@flomation.co>
2026-03-05 14:12:12 +0000

    fix: flo-166: removed hard coded credentials

--- internal/smtp/service_test.go
+++ internal/smtp/service_test.go
@@ -1,6 +1,9 @@
 package smtp
 
 import (
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	"flomation.app/sentinel/internal/config"
@@ -10,22 +13,30 @@ import (
 func TestGenericEmail(t *testing.T) {
 	RegisterTestingT(t)
 
+	username := os.Getenv("SENTINEL_TEST_SMTP_USERNAME")
+	password := os.Getenv("SENTINEL_TEST_SMTP_PASSWORD")
+	sendFrom := os.Getenv("SENTINEL_TEST_SMTP_SEND_FROM")
+	host := os.Getenv("SENTINEL_TEST_SMTP_HOST")
+	port, _ := strconv.ParseInt(os.Getenv("SENTINEL_TEST_SMTP_PORT"), 10, 64)
+
+	fmt.Printf("User: %v\nPassword: %v\nSend From: %v\nHost: %v\nPort: %v\n", username, password, sendFrom, host, port)
+
 	s := New(&config.Config{
 		Listener: config.ListenerConfig{
 			URL: "localhost",
 		},
 		Notification: config.NotificationConfig{
 			Enabled:  true,
-			SendFrom: "Flomation <hello@flomation.co>",
+			SendFrom: sendFrom,
 			SMTP: config.SMTPConfig{
-				Host:     "smtp-relay.gmail.com",
-				Username: "andy@flomation.co",
-				Password: "zebg wpjd fnmd yfex",
-				Port:     587,
+				Host:     host,
+				Username: username,
+				Password: password,
+				Port:     port,
 			},
 		},
 	})
 
-	err := s.SendTemplatedEmail("andy@flomation.co", "Test Email", "Testing Flomation Emails", "This is just a simple test of the Flomation Sentinel Email service - please ignore", "Open", "https://www.flomation.co")
+	err := s.SendTemplatedEmail("hello@flomation.co", "Test Email", "Testing Flomation Emails", "This is just a simple test of the Flomation Sentinel Email service - please ignore", "Open", "https://www.flomation.co")
 	Expect(err).To(BeNil())
 }
